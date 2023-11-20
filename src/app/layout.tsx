b1c98ab09c29c678792179142cfd4045e9a21795
Lucas Aprigio <lucsaprigio@hotmail.com>
2023-11-20 13:38:27 -0300

    App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 const roboto = Roboto({
   weight: '400',
