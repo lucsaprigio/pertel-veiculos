cded771b938094e4b0569225313f2f644468ace8
Lucas Aprigio <lucsaprigio@hotmail.com>
2023-11-20 23:10:31 -0300

    App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
       <div id="carros-estoque-destaque" className="flex flex-col gap-4 px-14 py-10 items-start justify-start w-full max-h-screen">
         <h2 className="text-4xl text-red-700 font-bold mt-8 font-inter">Novidades</h2>
-        <div className="grid grid-cols-2 w-full h-screen">
+        <div className="grid grid-cols-2 w-full">
           <div className="flex items-start justify-start w-full">
             <PromotionCard
               large={true}
@@ -75,12 +75,12 @@ export default function Home() {
             />
           </div>
         </div>
-      </div>
 
-      <div className="flex items-center justify-center w-full my-10 h-8 p-8">
-        <Link href="/veiculos" >
-          <button className=" p-4 bg-red-700 text-gray-150 text-xl rounded-md items-center justify-center hover:brightness-95 transition-all duration-150">Conheça nosso estoque</button>
-        </Link>
+        <div className="flex items-center justify-center w-full h-32 p-8">
+          <Link href="/veiculos" >
+            <button className=" p-4 bg-red-700 text-gray-150 text-xl rounded-md items-center justify-center hover:brightness-95 transition-all duration-150">Conheça nosso estoque</button>
+          </Link>
+        </div>
       </div>
 
       <div className="flex items-center justify-center w-full h-screen my-14">
