6bbad30ecec0f15a3ae011c981b3f626b34bcf0f
Lucas Aprigio <lucsaprigio@hotmail.com>
2023-12-03 10:08:37 -0300

    Auth path

--- src/app/(admin-routes)/painel/register-car/page.tsx
+++ src/app/(admin-routes)/painel/register-car/page.tsx
@@ -1,4 +1,4 @@
-import { nextAuthOptions } from '@/app/api/auth/[...nextauth]/route';
+import { nextAuthOptions } from '@/app/api/auth/[...nextauth]/auth';
 import { PlusCircle } from 'lucide-react';
 import { getServerSession } from 'next-auth';
 import NewCarForm from '../../Components/NewCarForm';
